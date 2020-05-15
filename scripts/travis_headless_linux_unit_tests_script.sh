72890b02887b54973fc00c14d38d26e5a729a932
mattRedBox <matt@redboxresearchdata.com.au>
2020-05-15 13:09:04 +1000

    Added all previous apt-get libraries after unit test couldn't find store on travis.

--- scripts/travis_headless_linux_unit_tests_script.sh
+++ scripts/travis_headless_linux_unit_tests_script.sh
@@ -7,7 +7,7 @@ sudo rm -Rf rm /tmp/.X*
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 export DISPLAY=':99.0'
 sudo apt-get update -y
-sudo apt-get install -y libgconf2-dev
-#sudo apt-get install -y libgtk-3-dev libgconf2-dev libnss3 libasound2 libxtst-dev libx11-xcb-dev libxss-dev
+#sudo apt-get install -y libgconf2-dev
+sudo apt-get install -y libgtk-3-dev libgconf2-dev libnss3 libasound2 libxtst-dev libx11-xcb-dev libxss-dev
 yarn run unit
 set +e
